17ee19f822204acc8db047ea96b3b0bb7efdb674
Philip Ince <philip.ince@rummble.com>
2014-03-02 22:27:43 +0000

    fixing hover on node issue

--- public/javascripts/zk_frontend.js
+++ public/javascripts/zk_frontend.js
@@ -17,7 +17,7 @@ function showSubTree(parent, children, dontAnimate) {
     parent.addClass("open");
 
     children.show('fast');
-    $(this).attr('title', 'Collapse this branch').find(' > i').addClass('glyphicon-minus').removeClass('glyphicon-plus');
+    $(this).attr('title', 'Collapse this branch').find('> i').addClass('glyphicon-minus').removeClass('glyphicon-plus');
 }
 function hideSubTree(parent, children, possibleChild) {
     var childrenSpan = $(this).siblings("span");
@@ -29,16 +29,27 @@ function hideSubTree(parent, children, possibleChild) {
     parent.next().removeClass("after-open");
     if(!possibleChild || parent.has(possibleChild).length==0){
         children.hide('fast');
-        $(this).attr('title', 'Expand this branch').find(' > i').addClass('glyphicon-plus').removeClass('glyphicon-minus');
+        $(this).attr('title', 'Expand this branch').find('> i').addClass('glyphicon-plus').removeClass('glyphicon-minus');
     }
 }
+function nodeHover(){
+    var parent = $(this).parent('li.parent_li');
+    parent.children('span.data, span.title').addClass('hovered');
+    parent.children('ul').find('li > span.title').addClass('hovered');
+}
+function nodeHoverOut(){
+    var parent = $(this).parent('li.parent_li');
+    parent.children('span').removeClass('hovered');
+    parent.children('ul').find('li > span.title').removeClass('hovered');
+}
+
 function resizeViewport() {
     var parent = $(currentlyOpen).parent('li.parent_li');
     var children = parent.find(' > ul > li');
     showSubTree.call(currentlyOpen, parent, children, true);
 }
 $(function () {
-    $('.tree li:has(ul)').addClass('parent_li').find(' > span').attr('title', 'Collapse this branch');
+    $('.tree li:has(ul)').addClass('parent_li').find('> span').attr('title', 'Collapse this branch');
     $('.tree li.parent_li > span').on('click', function (e) {
         var parent = $(this).parent('li.parent_li');
         var children = parent.find(' > ul > li');
@@ -55,6 +66,7 @@ $(function () {
         }
         e.stopPropagation();
     });
-    $('.tree li.parent_li > span').parent('li.parent_li').find(' > ul > li').hide('fast');
+    $('.tree li.parent_li > span').parent('li.parent_li').find('> ul > li').hide('fast');
     $(window).resize(resizeViewport);
+    $('.tree li.parent_li > span.title').hover(nodeHover, nodeHoverOut);
 });
