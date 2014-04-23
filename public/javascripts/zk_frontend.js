f84a7f4e1cb3b188d25441cb82128bc072d16e42
Philip Ince <fireman.phil@gmail.com>
2014-04-23 17:27:04 +0100

    fixed bug where clicking on data would minize the node to just the data

--- public/javascripts/zk_frontend.js
+++ public/javascripts/zk_frontend.js
@@ -73,7 +73,7 @@ function resizeViewport() {
 }
 $(function () {
     $('.tree li').addClass('parent_li').find('> span').attr('title', 'Collapse this branch');
-    $('.tree li.parent_li > span').on('click', function (e) {
+    $('.tree li.parent_li > span.title').on('click', function (e) {
         var parent = $(this).parent('li.parent_li');
         var children = parent.find(' > ul > li');
         if (children.is(":visible")) {
